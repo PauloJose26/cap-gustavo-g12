33a98fa8c78e0dd5fa71dd1ef20b3d9bd290d494
PauloJose26 <paulojoliveira.2616@gmail.com>
2022-04-28 10:51:05 -0300

    refactor: mudanças no para decimal nos valores do bids e products

--- app/models/products.py
+++ app/models/products.py
@@ -10,7 +10,7 @@ class Product(Model):
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
     name = Column(String(60), nullable=False)
     description = Column(String(1500), nullable=False)
-    starting_price = Column(Numeric(asdecimal=False), nullable=False)
+    starting_price = Column(Numeric(asdecimal=True), nullable=False)
     auction_start = Column(DateTime)
     auction_end = Column(DateTime)
     active = Column(Boolean, nullable=False, default=True)
