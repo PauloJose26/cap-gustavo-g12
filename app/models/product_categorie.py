e76df4bd4570cdc1bd362654a1cf231c8f507139
Johnny Gregório <36858209+johkker@users.noreply.github.com>
2022-04-28 19:53:34 -0300

    Update product_categorie.py
    Adicionado dataclasses faltantes em id_product e id_categorie

--- app/models/product_categorie.py
+++ app/models/product_categorie.py
@@ -7,6 +7,8 @@ from uuid import uuid4
 @dataclass
 class ProductCategorie(Model):
     id: int
+    id_product: int
+    id_categorie: int
     __tablename__ = "product_categorie"
     
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
